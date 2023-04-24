7eb7b1282ff2353c6bf51a545974564e94c48f75
Amanda Silva <106178378+amandasilvart@users.noreply.github.com>
2023-04-24 12:00:41 -0300

    Create Atividade4Controller.js

---
+++ Atividade4Controller.js
@@ -0,0 +1,18 @@
+({
+	showToast : function(component, event, helper) {
+    var toastEvent = $A.get("e.force:showToast");
+    toastEvent.setParams({
+        "title": "Success!",
+        "message": "Criado com sucesso.",
+        "type": "Success"
+    });
+    toastEvent.fire();
+    $A.get('e.force:refreshView').fire();
+},
+    handleReset: function(cmp, event, helper) {
+        cmp.find('field').forEach(function(f) {
+            f.reset();
+        });
+    }
+
+})
