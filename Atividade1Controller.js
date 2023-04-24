c6d0acc6bf44ee451cf044e37e67997a8b650277
Amanda Silva <106178378+amandasilvart@users.noreply.github.com>
2023-04-24 11:59:08 -0300

    Update Atividade1Controller.js

--- Atividade1Controller.js
+++ Atividade1Controller.js
@@ -1,11 +1,19 @@
 ({
-	showToast : function(component, event, helper) {
+showToast : function(component, event, helper) {
     var toastEvent = $A.get("e.force:showToast");
     toastEvent.setParams({
         "title": "Success!",
-        "message": "Cadastrado com sucesso."
+        "message": "Lead cadastrado com sucesso.",
+        "type": "Success"
     });
     toastEvent.fire();
-}
+    $A.get('e.force:refreshView').fire();
+},
+    handleReset: function(cmp, event, helper) {
+        cmp.find('field').forEach(function(f) {
+            f.reset();
+        });
+    }
+
 
 })
